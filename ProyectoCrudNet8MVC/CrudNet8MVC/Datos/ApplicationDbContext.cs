1e19d0982f1b9bc2af5b65861456b3a42e8e3876
yaro-dev <yarowebdev@gmail.com>
2025-01-13 16:12:47 -0600

    Project created first commit

---
+++ ProyectoCrudNet8MVC/CrudNet8MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet8MVC.Datos
+{
+    public class ApplicationDbContext:DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
